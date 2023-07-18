1a92dccb01e1657686d571720f64d6ca3ecc1734
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-07-18 11:37:31 +0200

    Movement achieved

--- SnapDoom/Assets/_Content/Scripts/Player/Movement.cs
+++ SnapDoom/Assets/_Content/Scripts/Player/Movement.cs
@@ -9,6 +9,7 @@ public class Movement : MonoBehaviour
 
     // Player Components
     private Transform cameraTransform;
+    private Rigidbody rb;
 
     // Debug
     public GameObject moveDebugCube;
@@ -16,33 +17,69 @@ public class Movement : MonoBehaviour
     void Awake()
     {
         cameraTransform = Camera.main.GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        // Move forward and backwards
         if(Input.GetKeyDown(KeyCode.W))
         {
-            ThrowRaycastToMove(cameraTransform.position, cameraTransform.forward);
+            ThrowRaycastToMove(cameraTransform.position, cameraTransform.forward, 1);
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            ThrowRaycastToMove(cameraTransform.position, cameraTransform.forward, -1);
+        }
+        else if(Input.GetKeyDown(KeyCode.A)) // Rotate
+        {
+            RoatatePlayer(-1f);
+        }
+        else if(Input.GetKeyDown(KeyCode.D)) // Rotate
+        {
+            RoatatePlayer(1f);
         }
     }
-    public RaycastHit ThrowRaycastToMove(Vector3 origin, Vector3 direction)
+    public RaycastHit ThrowRaycastToMove(Vector3 origin, Vector3 direction, int fb)
     {
         RaycastHit hit;
 
         if(Physics.Raycast(origin, direction, out hit, gameVariables.moveDistance))
         {
-            // if code is here means that the raycast has touched smthing and must raycast to ground to move
             if(debug)
                 Instantiate(moveDebugCube, hit.point, Quaternion.identity);
-            
-            // throw raycast to ground to move
-            ThrowRaycastToMove(hit.point, Vector3.down);
+            ThrowRaycastToMove(hit.point, -hit.transform.up, 0);
         }
         else
         {
-            Debug.Log("No Surface detected");
+            MovePlayerToPosition(new Vector3(transform.position.x, transform.position.y, transform.position.z+gameVariables.moveDistance), fb);
         }
 
         return hit;
+    }
+    public void MovePlayerToPosition(Vector3 finalPosition, int fb)
+    {
+        Vector3 direction = finalPosition - transform.position;
+        Vector3 normalizedDirection = direction.normalized;
+        if(fb < 0)
+        {
+            rb.AddForce(gameVariables.moveVelocity * -transform.forward, ForceMode.VelocityChange);
+        }
+        else
+        {
+            rb.AddForce(gameVariables.moveVelocity * transform.forward, ForceMode.VelocityChange);
+        }
+        
+        StartCoroutine(StopPlayer(gameVariables.timeToWait));
+    }
+    public IEnumerator StopPlayer(float s)
+    {
+        yield return new WaitForSeconds(s);
+        rb.velocity = Vector3.zero;
+    }
+
+    public void RoatatePlayer(float lr)
+    {
+        transform.Rotate(new Vector3(0f,gameVariables.turnDegrees*lr,0f), Space.Self);
     }
 }
--- SnapDoom/Assets/_Content/Scripts/Settings/GameVariables.cs
+++ SnapDoom/Assets/_Content/Scripts/Settings/GameVariables.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu]
 public class GameVariables : ScriptableObject
 {
-    public float moveDistance = 100f;
+    public float moveDistance = 10f;
     public float turnDegrees = 90f;
+    public float moveVelocity = 30f;
+    public float timeToWait = 0.15f; // time to wait when moving player corroutine
 }
