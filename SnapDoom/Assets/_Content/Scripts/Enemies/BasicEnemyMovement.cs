350370bf3c56a16fd9792ff5ddd2a8e3312bdce4
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-07-20 09:16:11 +0200

    added simple enemies

---
+++ SnapDoom/Assets/_Content/Scripts/Enemies/BasicEnemyMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class BasicEnemyMovement : MonoBehaviour
+{
+    private Animator anim;
+    private Transform player;
+    private NavMeshAgent agent;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.velocity = new Vector3(0.15f, 0.15f, 0.15f);
+
+        LookAtPlayer();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateDistanceToPlayer();
+        LookAtPlayer();
+    }
+    public void CalculateDistanceToPlayer()
+    {
+        float d = Vector3.Distance(transform.position, player.position);
+        anim.SetFloat("_DistanceToPlayer", d);
+        if(d < 25f)
+        {
+            agent.SetDestination(player.position);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+        }
+        Debug.Log("This is the distance to player: " + d);
+    }
+    private void LookAtPlayer()
+    {
+        transform.LookAt(player);
+    }
+}
